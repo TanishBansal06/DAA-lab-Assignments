343e21f0747ba57ed8dbda7a540100baabbfe547
Tanish Bansal <tanishbansal0624@gmail.com>
2026-01-13 14:31:24 +0530

    Update q1.cpp

--- assignment1/q1.cpp
+++ assignment1/q1.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
-
+int binarySearch(int arr[], int l, int h, int tar) {
+    if (l > h) {
+        return -1;
+    }
+    int mid = l + (h - l) / 2;
+    if (arr[mid] == tar)
+        return mid;
+    else if (arr[mid] > tar)
+        return binarySearch(arr, l, mid - 1, tar);
+    else
+        return binarySearch(arr, mid + 1, h, tar);
+}
 int main() {
-   int arr[10] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
-   int l=0;
-   int h=9;
-   int tar=23;
-   while(l<=h){
-       int mid = (l+h)/2;
-       if(arr[mid]==tar){
-           cout<<"target is found at index: "<<mid<<endl;
-           break;
-       }
-       else if(arr[mid]>tar){
-           h=mid-1;
-       }
-       else{
-           l=mid+1;
-       }
-   }
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int tar;
+    cin >> tar;
+    int result = binarySearch(arr, 0, n - 1, tar);
+    if (result != -1)
+        cout << result << endl;
+    else
+        cout << -1 << endl;
+    return 0;
 }
